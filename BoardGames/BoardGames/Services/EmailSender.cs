01085f218d9dceb70baea6eadbeeef1cf38bc776
Zack Ruppert <zackruppert@gmail.com>
2018-01-02 16:06:44 -0500

    Add base solution & project

---
+++ BoardGames/BoardGames/Services/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using BoardGames.Models;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGames.Services
+{
+    // This class is used by the application to send email for account confirmation and password reset.
+    // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailSender(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            using (var client = new HttpClient { BaseAddress = new Uri(_emailSettings.BaseUri) })
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                    Convert.ToBase64String(Encoding.ASCII.GetBytes(_emailSettings.ApiKey)));
+
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("from", _emailSettings.From),
+                    new KeyValuePair<string, string>("to", email),
+                    new KeyValuePair<string, string>("subject", subject),
+                    new KeyValuePair<string, string>("text", message)
+                });
+
+                await client.PostAsync(_emailSettings.RequestUri, content).ConfigureAwait(false);
+            }
+        }
+    }
+}

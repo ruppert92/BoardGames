fa4b88758a03f8fc09740199b08ed95334165bed
ruppert92 <zackruppert@gmail.com>
2018-01-10 22:04:49 -0500

    Run migration scripts for first time

--- BoardGames/BoardGames/Startup.cs
+++ BoardGames/BoardGames/Startup.cs
@@ -27,7 +27,7 @@ namespace BoardGames
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
